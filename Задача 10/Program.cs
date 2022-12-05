deb57b563f7856b968235ded903194d4deb892a9
Marisabell <mary_tusa@mail.ru>
2022-12-06 01:47:45 +0400

    Решение

--- Задача 10/Program.cs
+++ Задача 10/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine());
+int firstPart = number/10;
+int secondPart = firstPart%10;
+Console.WriteLine(secondPart);
