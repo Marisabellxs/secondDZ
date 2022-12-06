2b4e5b02199a28140d4a662b4b61867da234fc80
Marisabell <mary_tusa@mail.ru>
2022-12-07 00:58:58 +0400

    Решение 15

---
+++ Задача 15/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+switch (number)
+{
+    case 1: Console.WriteLine("Понедельник");
+    break;
+    case 2: Console.WriteLine("Вторник");
+    break;
+    case 3: Console.WriteLine("Среда");
+    break;
+    case 4: Console.WriteLine("Четверг");
+    break;
+    case 5: Console.WriteLine("Пятница");
+    break;
+    case 6: Console.WriteLine("Суббота");
+    break;
+    case 7: Console.WriteLine("Воскресенье");
+    break;
+    default:
+    break;
+}
+   if (number==6 || number==7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
